23afd5514037ca86935f8a76b2875990a4d3f8cf
lincklopescarol <lincklopescarol@gmail.com>
2023-11-21 12:52:30 -0300

    refatorando a classe Musica e inicializando o objeto Musica

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Musica musica1 = new Musica();
+
+musica1.nome = "Perfect";
+musica1.artista = "Ed Sheeran";
+Console.WriteLine($"Nome da música: {musica1.nome}");
+Console.WriteLine($"Artista: {musica1.artista}");
+
+Musica musica2 = new Musica();
+musica2.nome = "Matilda";
+musica2.artista = "Harry Styles";
