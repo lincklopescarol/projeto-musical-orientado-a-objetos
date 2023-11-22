50b7068165a33a2db9b6b2de3c6cdd60fc32753e
lincklopescarol <lincklopescarol@gmail.com>
2023-11-22 11:00:06 -0300

    adicionando get e set e adicionando lambda

--- Musica.cs
+++ Musica.cs
@@ -1,10 +1,11 @@
 class Musica
 {
-    public string Nome {get; set;};
-    public string Artista {get; set;} ;
-    public int Duracao {get; set;};
-    public bool Disponivel {get; set;};
-    public string NomeCompleto {get; set;};
+public string Nome { get; set; }
+public string Artista { get; set; }
+public int Duracao { get; set; }
+public bool Disponivel { get; set; }
+public string DescricaoResumida => 
+    $"A música {Nome} pertence à banda {Artista}";
 
     public void ExibirFichaTecnica()
     {
