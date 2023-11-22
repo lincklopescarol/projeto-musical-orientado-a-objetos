dcd34a2ff727c3d160df795b180d7d7f0737fc5f
lincklopescarol <lincklopescarol@gmail.com>
2023-11-22 09:54:22 -0300

    refatorando as classes Musica e Program e colocando os métodos get e set

--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
 ﻿Musica musica1 = new Musica();
 
-musica1.nome = "Perfect";
-musica1.artista = "Ed Sheeran";
-musica1.duracao = 350;
-musica1.EscreveDisponivel(true);
-Console.WriteLine(musica1.LeDisponivel());
+musica1.Nome = "Perfect";
+musica1.Artista = "Ed Sheeran";
+musica1.Duracao = 350;
+musica1.Disponivel = true;
+Console.WriteLine(musica1.Disponivel());
 
 Musica musica2 = new Musica();
-musica2.nome = "Matilda";
-musica2.artista = "Harry Styles";
-musica2.duracao = 300;
-musica1.EscreveDisponivel(false);
-Console.WriteLine(musica1.LeDisponivel());
+musica2.Nome = "Matilda";
+musica2.Artista = "Harry Styles";
+musica2.Duracao = 300;
+musica2.Disponivel = false;
+Console.WriteLine(musica2.Disponivel());
 
 musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
